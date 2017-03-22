2ae1ec25f76242f188bf6528526d595eb835534b
Richard Sun <jeffsun@Jeffs-MacBook.local>
2017-03-21 21:20:30 -0400

    Initial Commit

---
+++ FunFacts/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FunFacts
+//
+//  Created by Richard Sun on 2017-03-21.
+//  Copyright © 2017 Richard Sun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
